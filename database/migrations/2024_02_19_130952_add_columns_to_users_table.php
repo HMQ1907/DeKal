36c76757cc2222fea08aac6bd7ef11da0a02d6a0
MSI\QuangHM <hominhquang01@gmail.com>
2024-02-20 18:45:12 +0700

    add migration + seeder user table

---
+++ database/migrations/2024_02_19_130952_add_columns_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->timestamp('date_work_start')->default(Date('Y-m-d'));
+            $table->string('family_register')->nullable();
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    User::create([
+        'name' => 'User',
+        'email' => 'user@example.com',
+        'user_type' => 'user', 
+        'password' => bcrypt('password'),
+    ]);
+
+    User::create([
+        'name' => 'Admin',
+        'email' => 'admin@example.com',
+        'user_type' => 'admin', 
+        'password' => bcrypt('password'),
+    ]);
+
     }
 }
